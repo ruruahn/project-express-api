0155006c1fe74939606fb9e4dd20650acdd4bdfb
Haru Ahn <81482699+ruruahn@users.noreply.github.com>
2021-12-10 07:50:54 +0100

    added json GET and first query

--- server.js
+++ server.js
@@ -1,33 +1,65 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+// importing the JSON from the books.json
+import booksData from "./data/books.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Defines the port the app will run on. Defaults to 8080, but can be
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-// Start the server
+// gets all books
+app.get("/books", (req, res) => {
+  const { author } = req.query;
+
+  //get a list of books with specific authors
+  let sortByAuthor = booksData;
+  if (author) {
+    sortByAuthor = sortByAuthor.filter((item) => item.authors.toLowerCase().includes(author.toLowerCase()));
+  }
+  res.json({
+    response: sortByAuthor,
+    success: true,
+  });
+});
+
+//returns one book with the specified bookID-number
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const myBook = booksData.find((item) => item.bookID == +id);
+  //returns error message when there is no requested id
+  if (!myBook) {
+    res.status(404).send("No Book with that id");
+  } else {
+    res.json(myBook);
+  }
+});
+
+// returns an array of books from a specified author
+app.get("/authors/:author", (req, res) => {
+  const author = req.params.author;
+  const showAuthor = booksData.filter((item) => item.authors === author);
+  res.json(showAuthor);
+});
+
+// returns one book with the specified ISBN-number
+app.get("/isbn/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const myIsbn = booksData.filter((item) => item.isbn === +isbn);
+  res.json(myIsbn);
+});
+
+// Start the server on 8080 local server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
